3e2110030de9fff5fc6f3cd6b36981ae447651d2
Anson <anson32330200@gmail.com>
2017-01-22 13:44:31 -0800

    feat: Add vectors

---
+++ C2S4L11_Vectors/src/C2S4L11_Vectors.cpp
@@ -0,0 +1,30 @@
+//============================================================================
+// Name        : C2S4L11_Vectors.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	vector<string> strs(3);
+	strs.push_back("one");
+	strs.push_back("two");
+	strs.push_back("three");
+	cout << strs.size() << endl;	// outputs 6
+
+	for (int i = 0; i < strs.size(); i++)
+		cout << strs[i] << endl;
+
+	for (vector<string>::iterator it = strs.begin(); it != strs.end(); it++)
+		cout << *it << endl;
+
+	vector<string>::iterator it = strs.begin();
+	it += 3;
+	cout << *it;	// outputs "one"
+	return 0;
+}
